442379b10f23bd3dd64a2766802475498e082511
Junio28 <ejuniormartinez@uniguajira.edu.co>
2022-03-23 15:40:32 -0500

    Paso 23

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { GameFormComponent } from './components/game-form/game-form.component';
+import { GameListComponent } from './components/game-list/game-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    GameFormComponent,
+    GameListComponent
   ],
   imports: [
     BrowserModule,
